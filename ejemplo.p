f2c578695bcd6a6ed2d11b282aea9cb395cabab1
ADAMS <ADAMS@ADAMS-PC>
2012-11-05 21:23:23 -0430

    comparacion MENOR y MAYOR

--- ejemplo.p
+++ ejemplo.p
@@ -1,17 +1,8 @@
 PROGRAM prueba;
-	VAR
-		X1,X2,Y1,Y2:INTEGER;
-		W1:BOOLEAN;
 	BEGIN
-		X1:=177;
-		IF 2=2 THEN 
+		IF 2>1 THEN 
 			BEGIN
 				WRITE (77);
 				WRITE (771);
-			END
-		ELSE
-			BEGIN
-				WRITE (88);
-				WRITE (X1);
 			END;
 END.
--- src/Compilador.java
+++ src/Compilador.java
@@ -1,8 +1,7 @@
 
-import util.TablaSimbolos;
 import ast.*;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import util.TablaSimbolos;
 import util.UtGen;
 
 /**
@@ -330,8 +329,22 @@ public class Compilador {
 
         switch (nodo.getTipo()) {
             case IGUAL:
-                UtGen.emitirRO("SUB", UtGen.AC, UtGen.AC1, UtGen.AC, "op: =",out);
-                UtGen.emitirRM("JEQ", UtGen.AC, 2, UtGen.PC, "voy dos instrucciones mas alla if verdadero (AC==0)",out);
+                UtGen.emitirRO("SUB", UtGen.AC, UtGen.AC1, UtGen.AC, "op: =", out);
+                UtGen.emitirRM("JEQ", UtGen.AC, 2, UtGen.PC, "voy dos instrucciones mas alla if verdadero (AC==0)", out);
+                UtGen.emitirRM("LDC", UtGen.AC, 0, UtGen.AC, "caso de falso (AC=0)", out);
+                UtGen.emitirRM("LDA", UtGen.PC, 1, UtGen.PC, "Salto incodicional a direccion: PC+1 (es falso evito colocarlo verdadero)", out);
+                UtGen.emitirRM("LDC", UtGen.AC, 1, UtGen.AC, "caso de verdadero (AC=1)", out);
+                break;
+            case MENOR:
+                UtGen.emitirRO("SUB", UtGen.AC, UtGen.AC1, UtGen.AC, "op: <",out);
+                UtGen.emitirRM("JLT", UtGen.AC, 2, UtGen.PC, "voy dos instrucciones mas alla if verdadero (AC<0)",out);
+                UtGen.emitirRM("LDC", UtGen.AC, 0, UtGen.AC, "caso de falso (AC=0)",out);
+                UtGen.emitirRM("LDA", UtGen.PC, 1, UtGen.PC, "Salto incodicional a direccion: PC+1 (es falso evito colocarlo verdadero)",out);
+                UtGen.emitirRM("LDC", UtGen.AC, 1, UtGen.AC, "caso de verdadero (AC=1)",out);
+                break;
+            case MAYOR:
+                UtGen.emitirRO("SUB", UtGen.AC, UtGen.AC, UtGen.AC1, "op: >",out);
+                UtGen.emitirRM("JGT", UtGen.AC, 2, UtGen.PC, "voy dos instrucciones mas alla if verdadero (AC>0)",out);
                 UtGen.emitirRM("LDC", UtGen.AC, 0, UtGen.AC, "caso de falso (AC=0)",out);
                 UtGen.emitirRM("LDA", UtGen.PC, 1, UtGen.PC, "Salto incodicional a direccion: PC+1 (es falso evito colocarlo verdadero)",out);
                 UtGen.emitirRM("LDC", UtGen.AC, 1, UtGen.AC, "caso de verdadero (AC=1)",out);
